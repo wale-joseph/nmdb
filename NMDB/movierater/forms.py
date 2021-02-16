aefc247256a277463ba7f1798d73325efedd7f05
Wale Joseph <30349247+josoga2@users.noreply.github.com>
2021-02-16 09:57:01 +0300

    image_column_added

--- NMDB/movierater/forms.py
+++ NMDB/movierater/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
+
+
+validate_file = FileExtensionValidator(allowed_extensions=['jpg','png','jpeg'],
+                                        message='Wrong File Format',
+                                        code='200')
 
 class newMovie(forms.Form):
     Title = forms.CharField(max_length=128)
-    #coverArt = forms.ImageField()
+    coverArt = forms.FileField(validators=[validate_file])
     Comment = forms.CharField(max_length=200, widget=forms.Textarea)
--- NMDB/movierater/views.py
+++ NMDB/movierater/views.py
@@ -1,45 +1,58 @@
 from django.shortcuts import render
 from .forms import newMovie
-from keras.models import load_model
-from keras.preprocessing import image
+import tensorflow_datasets as tfds
+#from keras.preprocessing import image
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import json
 from tensorflow import Graph, Session
+import numpy as np
+from django.core.files.storage import FileSystemStorage
 
 
+def handle_uploaded_file_path(f):
+    stdout = 'movierater/static/uploads'+f.name
+    with open('movierater/static/uploads'+f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return str(stdout)
 
 model_graph = Graph()
 with model_graph.as_default():
     tf_session = Session()
     with tf_session.as_default():
-        model=load_model('/home/josoga2/Documents/projects/NMDB/MovieClassifier.h5')
+        model=load_model('/home/josoga2/Documents/projects/NMDB/MovieClassifier.h5', compile=False)
+
+encoder = tfds.deprecated.text.SubwordTextEncoder.load_from_file('/home/josoga2/Documents/projects/NMDB/encoder')
+print(encoder)
 
 # Create your views here.
 
 def ratingPrediction(request):
 
-    form = newMovie(request.POST)
-    
-    def pad_to_size(vec, size):
-        zeros = [0]*(size-len(vec))
-        vec.extend(zeros)
-        return vec
-
-    def sample_predict(sentence, pad):
-        encoded_sample_pred_text = encoder.encode(sentence)
-        if pad:
-            encoded_sample_pred_text = pad_to_size(encoded_sample_pred_text, 64)
-            encoded_sample_pred_text = tf.cast(encoded_sample_pred_text, tf.float32)
-            predictions = model.predict(tf.expand_dims(encoded_sample_pred_text, 0))
-            return predictions
-    
-    sample_text = ('How wish i can stop falling in love with Nazo Ekezie')
+    form = newMovie(request.POST, request.FILES)
     sample_text = request.POST.get('Comment')
-    predictions = sample_predict(sample_text, pad=True)*10
-    print('Predicted review based on IMDB data%.2f' %predictions)
+    
+    coverArt = print(request.FILES.get('coverArt'))
+    
+    coverArt = request.FILES.get('coverArt')
+    print(coverArt)
+
+    #sample_text = ('How wish i can stop falling in love with Nazo Ekezie')
+    #predictions = sample_predict(sample_text, pad=True)*10
+    #print('Predicted review based on IMDB data%.2f' %predictions)
 
     movieName = request.POST.get('Title')
-    MovieRating = predictions
     
-    context = {'movieName':movieName, 'MovieRating': MovieRating, 'form':form}
+    with model_graph.as_default():
+        with tf_session.as_default():
+            outRes = model.predict((encoder.encode(str(sample_text))))
+    
+    
+    print(encoder)
+   
+    #predictedLabel=encoder[str(np.array(outRes))]
+    MovieRating = round(max(outRes)[0]*10, 1)
+    
+    context = {'movieName':movieName, 'MovieRating': MovieRating, 'form':form, 'coverArt':coverArt}
     return render(request, 'index.html', context)
